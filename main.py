da28c8a363e3d853e22c31c4f739261a56ec1a4a
Roberto <roberfuentesg@gmail.com>
2022-12-28 13:36:30 -0300

    feat: add scraping html class

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-scraper = pd.read_html('https://www.mtggoldfish.com/archetype/pioneer-mono-white-humans#paper')
+url = 'https://www.mtggoldfish.com/archetype/pioneer-mono-white-humans#paper'
 
-for index, table in enumerate(scraper):
+scraper = pd.read_html(url)
+
+response = requests.get(url)
+bs = BeautifulSoup(response.text, "html.parser")
+title = bs.find(class_="title").text.split("\n")[1]
+format = bs.find(class_="deck-container-information").text.split("\n")[1]
+
+""" for index, table in enumerate(scraper):
     print('*************')
     print(index)
-    print(table)
+    print(table) """
 
+print("Deck: " + title)
+print(format)
 print(scraper[0])
