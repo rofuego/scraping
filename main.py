cf2a47b9bb49a1a280bf14f1cce00a185c77a683
Roberto <roberfuentesg@gmail.com>
2022-12-28 05:09:55 -0300

    chore: remove comments

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 scraper = pd.read_html('https://www.mtggoldfish.com/archetype/pioneer-mono-white-humans#paper')
 
-""" for index, table in enumerate(scraper):
+for index, table in enumerate(scraper):
     print('*************')
     print(index)
     print(table)
- """
 
 print(scraper[0])
